a7a5448c5f9f1e621794168b992c928b8d56a914
dusk-till-dawn-6946 <pratik.official6946@gmail.com>
2025-08-20 03:08:00 +0530

    Create Mobile_Shop.java

---
+++ src/Mobile_Shop.java
@@ -0,0 +1,12 @@
+import smart.Smartphone;
+
+
+public class Mobile_Shop {
+    public static void main(String[] args) {
+        Smartphone sp = new Smartphone();
+        System.out.println("Rajib Kuri, Roll No: 42");
+        sp.display();
+        sp.purchase();
+        sp.display();
+    }
+}
